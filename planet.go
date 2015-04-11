4884681aa2f871af517603cb9ffddc9606fc9edb
ik5 <idok@linesip.com>
2015-04-11 21:02:03 +0300

    just basics nothing to see here, move alone

--- planet.go
+++ planet.go
@@ -3,13 +3,12 @@
 package main
 
 import (
-	"github.com/SlyMarbo/rss"
-	//"os"
-	"time"
+//"os"
+//"time"
 )
 
 func main() {
-	var sites = []SiteRec{}
-	var entries = map[time.Time][]PostEntry{}
+	//var sites = []SiteRec{}
+	//var entries = map[time.Time][]PostEntry{}
 
 }
