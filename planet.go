ca12ec234285d457ea0d199dc06b42a9a10d3795
ik5 <idok@linesip.com>
2015-04-11 17:36:23 +0300

    working better, no global variables

--- planet.go
+++ planet.go
@@ -8,16 +8,8 @@ import (
 	"time"
 )
 
-var sites = []SiteRec{}
-var entries = map[time.Time][]PostEntry{}
+func main() {
+	var sites = []SiteRec{}
+	var entries = map[time.Time][]PostEntry{}
 
-func ParseSite(address string) error {
-	_, err := rss.Fetch("http://example.com/rss")
-	if err != nil {
-		return err
-	}
-
-	//feed.Title
-
-	return nil
 }
