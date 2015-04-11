7fe114c7e5961aba697b33093784ca813751bbee
ik5 <idok@linesip.com>
2015-04-11 22:22:32 +0300

    starting to work on parsing the settings

--- settings.go
+++ settings.go
@@ -1,34 +1,58 @@
 package main
 
-/*
 import (
 	ini "github.com/vaughan0/go-ini"
 )
-*/
 
 type Settings struct {
-	Planet struct {
-		SiteName     string    // The ttle of the site
-		DisplayURL   string    // The url to be displayd
-		OwnerName    string    // The owner of the site
-		OwnerEmail   string    // The owner email
-		ItemsPerPage int       // The number of items at a page
-		PublicDir    string    // The path to generate the content into
-		TemplateDir  string    // The path of the templates
-		TemplateName [5]string /*
-			    The name of the templates to use:
-					   0. index
-					   1. atom
-					   2. rss20
-					   3. rss10
-					   4. opml
-		*/
-	}
+	SiteName     string    // The ttle of the site
+	DisplayURL   string    // The url to be displayd
+	OwnerName    string    // The owner of the site
+	OwnerEmail   string    // The owner email
+	ItemsPerPage int       // The number of items at a page
+	PublicDir    string    // The path to generate the content into
+	TemplateDir  string    // The path of the templates
+	TemplateName [5]string /*
+		The name of the templates to use:
+		0. index
+		1. atom
+		2. rss20
+		3. rss10
+		4. opml
+	*/
 
 	Sites []SiteRec
 }
 
 func (Settings) LoadConf(filename string) (Settings, error) {
+	file, err := ini.LoadFile(filename)
+	if err != nil {
+		return Settings{}, err
+	}
+
+	var settings Settings
+
+	for name, _ := range file {
+		for key, value := range file[name] {
+			switch name {
+			case "settings":
+				switch key {
+				case "site_name":
+					settings.SiteName = value
+				case "display_url":
+					settings.DisplayURL = value
+				case "owner_name":
+					settings.OwnerName = value
+				case "owner_email":
+					settings.OwnerEmail = value
+				}
+			//case "default":
+
+			default:
+			}
+		}
+	}
+
 	//site := AddSite(feed.Title, feed.Description, feed.Link, feed.UpdateURL, feed.Nickname, false)
 	return Settings{}, nil
 }
