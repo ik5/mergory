cf7ffcdae5f0db9be7e82b564fe008ca4fec9fbd
ik5 <idok@linesip.com>
2015-04-11 21:01:18 +0300

    fixed the way I used declerations

--- posts.go
+++ posts.go
@@ -1,6 +1,7 @@
-package posts
+package main
 
 import (
+	"github.com/SlyMarbo/rss"
 	"time"
 )
 
@@ -10,7 +11,7 @@ type SiteRec struct {
 	Site        string // Main site address e.g. http://blog.example.com/
 	Feed        string // The feed address e.g. http://blog.example.com/computers/feed/
 	Author      string // The name of the author for the site
-	Rtl         string // The main direction of the blog (Left to Right or Right to Left)
+	Rtl         bool   // The main direction of the blog (Left to Right or Right to Left)
 }
 
 type PostEntry struct {
@@ -24,7 +25,7 @@ type PostEntry struct {
 	Modified time.Time // The date and time of the Post
 }
 
-func (SiteRec) AddSite(Title, Description, Address, Feed, Author, Rtl) SiteRec {
+func AddSite(Title, Description, Address, Feed, Author string, Rtl bool) SiteRec {
 	return SiteRec{
 		Title,
 		Description,
@@ -35,7 +36,7 @@ func (SiteRec) AddSite(Title, Description, Address, Feed, Author, Rtl) SiteRec {
 	}
 }
 
-func (PostEntry) AddPost(Title, Body, Author, Url, ID, Rtl, Modified) PostEntry {
+func AddPost(Title, Body, Author, Url, ID string, Rtl bool, Modified time.Time) PostEntry {
 	return PostEntry{
 		Title,
 		Body,
@@ -53,24 +54,14 @@ func ParseSite(address string, sites *[]SiteRec) (map[time.Time][]PostEntry, err
 		return nil, err
 	}
 
-	site := SiteRec.AddSite(
-		feed.Title, feed.Description, feed.Link, feed.UpdateURL, feed.Author, false,
-	)
+	site := AddSite(feed.Title, feed.Description, feed.Link, feed.UpdateURL, feed.Nickname, false)
 
 	*sites = append(*sites, site)
 	entries := map[time.Time][]PostEntry{}
-	for _, v := range feed.Item {
-		entry := AddPost(
-			v.Title,
-			v.Content,
-			v.Nickname,
-			v.Link,
-			v.ID,
-			false,
-			v.Date,
-		)
+	for _, v := range feed.Items {
+		entry := AddPost(v.Title, v.Content, feed.Nickname, v.Link, v.ID, false, v.Date)
 
-		entries[v.Date] = append(entries[v.date], entry)
+		entries[v.Date] = append(entries[v.Date], entry)
 	}
 
 	return entries, nil
