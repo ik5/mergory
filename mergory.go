eab2c7e1108b8d18c71f4537fbc5faf4acb784ab
ik5 <idok@linesip.com>
2015-04-18 13:54:49 +0300

    doing some basic tests

--- mergory.go
+++ mergory.go
@@ -3,12 +3,19 @@
 package main
 
 import (
-//"os"
-//"time"
+	//"os"
+	//"time"
+	"fmt"
 )
 
 func main() {
 	//var sites = []SiteRec{}
 	//var entries = map[time.Time][]PostEntry{}
+	settings, err := LoadConf("config/test.ini")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%v\n", settings)
 
 }
